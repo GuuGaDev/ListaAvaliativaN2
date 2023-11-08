031cb85e76127566204c6640d7ac66c1e6b2fe68
Gustavo Daher <132029160+GuuGaDev@users.noreply.github.com>
2023-11-07 23:14:40 -0300

    Update 03.c

--- 03.c
+++ 03.c
@@ -1,53 +1,79 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-    int matrizA[4][4], matrizB[4][4], resultado[4][4];
-    char operacao[5];
-    int i, j, k;
-
-    for (i = 0; i < 4; i++) {
-        for (j = 0; j < 4; j++) {
-            scanf("%d", &matrizA[i][j]);
+void lerMatriz(int matriz[4][4]) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            scanf("%d", &matriz[i][j]);
         }
     }
+}
 
-    for (i = 0; i < 4; i++) {
-        for (j = 0; j < 4; j++) {
-            scanf("%d", &matrizB[i][j]);
-        }
-    }
-
-    scanf("%s", operacao);
-
-    if (operacao[0] == 's') {
-        for (i = 0; i < 4; i++) {
-            for (j = 0; j < 4; j++) {
-                resultado[i][j] = matrizA[i][j] + matrizB[i][j];
-            }
-        }
-    } else if (operacao[0] == 'm') {
-        for (i = 0; i < 4; i++) {
-            for (j = 0; j < 4; j++) {
-                resultado[i][j] = 0;
-                for (k = 0; k < 4; k++) {
-                    resultado[i][j] += matrizA[i][k] * matrizB[k][j];
-                }
-            }
-        }
-    } else if (operacao[0] == 's' && operacao[1] == 'u') {
-        for (i = 0; i < 4; i++) {
-            for (j = 0; j < 4; j++) {
-                resultado[i][j] = matrizA[i][j] - matrizB[i][j];
-            }
-        }
-    }
-
-    for (i = 0; i < 4; i++) {
-        for (j = 0; j < 4; j++) {
-            printf("%4d", resultado[i][j]);
+void imprimirMatriz(int matriz[4][4]) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%4d", matriz[i][j]);
         }
         printf("\n");
     }
+}
 
-    return 0;
+void somarMatrizes(int A[4][4], int B[4][4]) {
+    int resultado[4][4];
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            resultado[i][j] = A[i][j] + B[i][j];
+        }
+    }
+
+    imprimirMatriz(resultado);
+}
+
+void subtrairMatrizes(int A[4][4], int B[4][4]) {
+    int resultado[4][4];
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            resultado[i][j] = A[i][j] - B[i][j];
+        }
+    }
+
+    imprimirMatriz(resultado);
+}
+
+void multiplicarMatrizes(int A[4][4], int B[4][4]) {
+    int resultado[4][4];
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            resultado[i][j] = 0;
+            for (int k = 0; k < 4; k++) {
+                resultado[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    imprimirMatriz(resultado);
+}
+
+int main() {
+    int A[4][4], B[4][4];
+    char operacao[5];
+
+    lerMatriz(A);
+    lerMatriz(B);
+    scanf("%s", operacao);
+
+    if (strcmp(operacao, "soma") == 0) {
+        somarMatrizes(A, B);
+    } else if (strcmp(operacao, "sub") == 0) {
+        subtrairMatrizes(A, B);
+    } else if (strcmp(operacao, "mult") == 0) {
+        multiplicarMatrizes(A, B);
+    } else {
+        printf("Operação inválida\n");
+    }
+
+    return 0;
 }
